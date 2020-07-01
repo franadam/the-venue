53103c10d000c447dfc4a27b459ad389a93aa0a1
franadam <francoisadam@hotmail.be>
2020-07-01 23:55:38 +0200

    Updates

---
+++ precache-manifest.2bfbca7904beff0b4e303a7d56c18b76.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78485844d473ad8c6544e76afcb7d432",
+    "url": "/the-venue/index.html"
+  },
+  {
+    "revision": "274f39799f3013bd3ec8",
+    "url": "/the-venue/static/css/main.d7d7bf73.chunk.css"
+  },
+  {
+    "revision": "71778310605a8e3d6121",
+    "url": "/the-venue/static/js/2.b4851387.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/the-venue/static/js/2.b4851387.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "274f39799f3013bd3ec8",
+    "url": "/the-venue/static/js/main.1a827ade.chunk.js"
+  },
+  {
+    "revision": "fb7eebacd01a40c131cf",
+    "url": "/the-venue/static/js/runtime-main.ff11cd2a.js"
+  },
+  {
+    "revision": "84753208e3ace6c36a3a29c731983a69",
+    "url": "/the-venue/static/media/slide_one.84753208.jpg"
+  },
+  {
+    "revision": "9aa09a50b7eb84e96d522c10dfaa78e5",
+    "url": "/the-venue/static/media/slide_three.9aa09a50.jpg"
+  },
+  {
+    "revision": "3a3ffd3696fea8999087bbdcf97d69d2",
+    "url": "/the-venue/static/media/slide_two.3a3ffd36.jpg"
+  }
+]);
